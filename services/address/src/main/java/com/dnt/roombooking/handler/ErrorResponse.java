5e2e1f82c58ff5eeab8cd48f2159064502306220
ngoctudev2613@gmail.com <ngoctudev2613@gmail.com>
2025-05-07 10:58:35 +0700

    fix error on address services

--- services/address/src/main/java/com/dnt/roombooking/handler/ErrorResponse.java
+++
@@ -1,9 +0,0 @@
-package com.dnt.roombooking.handler;
-
-import java.util.Map;
-
-public record ErrorResponse(
-        Map<String, String> errors
-) {
-
-}
---
+++ services/address/src/main/resources/db/migration/V1__init_database.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS address (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    district VARCHAR(255),
+    city VARCHAR(255) NOT NULL
+);
+
+INSERT INTO address (name, district, city) VALUES
+('Chợ Bến Thành', 'Quận 1', 'TP HCM'),
+('Công viên Lê Văn Tám', 'Quận 3', 'TP HCM'),
+('Hồ Gươm', 'Hoàn Kiếm', 'Hà Nội'),
+('Phố cổ Hội An', 'Minh An', 'Hội An'),
+('Chùa Thiên Mụ', 'Hương Long', 'Huế');
