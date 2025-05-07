518093801d877d944beb51cad12f7bbaccb2ce03
ngoctudev2613@gmail.com <ngoctudev2613@gmail.com>
2025-05-07 14:39:40 +0700

    add config for accommodation service

---
+++ services/accommodation/src/test/java/com/dnt/roombooking/AccomodationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnt.roombooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccomodationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
